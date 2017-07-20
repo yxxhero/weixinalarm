f57955dd39656b8fd2a89404429f4e983e6fb4cb
root <root@bogon.(none)>
2017-07-20 14:23:49 +0800

    bug fix

--- weixinalarm.py
+++ weixinalarm.py
@@ -19,8 +19,8 @@ logging.basicConfig(level="DEBUG",
 class weixinalarm(object):
     def __init__(self,corpid,secrect,agentid):
         self.corpid=corpid
-	self.secrect=secrect
-	self.agentid=agentid
+        self.secrect=secrect
+        self.agentid=agentid
     def get_access_token(self):
         access_token_url="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid="+self.corpid+"&corpsecret="+self.secrect
         try:
