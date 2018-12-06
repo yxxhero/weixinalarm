04f515259ca597d3164e32170067a84eb05ec478
yxxhero <526169407@qq.com>
2018-12-06 20:58:42 +0800

    Update weixinalarm.py

--- weixinalarm.py
+++ weixinalarm.py
@@ -14,7 +14,7 @@ sys.setdefaultencoding('utf8')
 logging.basicConfig(level="DEBUG",
                 format='%(asctime)s  %(levelname)s %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S',
-                filename='./log/dark_status.log',
+                filename='./log/status.log',
                 filemode='a')
 class weixinalarm(object):
     def __init__(self,corpid,secrect,agentid):
