94b85e8d0c09fb5146163a12024332632b6d01dc
yxxhero <yuanxiongxiong@genedock.com>
2019-01-25 10:11:57 +0800

    pep8

--- weixinalarm.py
+++ weixinalarm.py
@@ -10,83 +10,96 @@ import os
 import logging
 reload(sys)
 sys.setdefaultencoding('utf8')
-#日志模式初始化
+# 日志模式初始化
 logging.basicConfig(level="DEBUG",
-                format='%(asctime)s  %(levelname)s %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S',
-                filename='./log/status.log',
-                filemode='a')
+                    format='%(asctime)s  %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+
 class weixinalarm(object):
-    def __init__(self,corpid,secrect,agentid):
-        self.corpid=corpid
-        self.secrect=secrect
-        self.agentid=agentid
+    def __init__(self, corpid, secrect, agentid):
+        self.corpid = corpid
+        self.secrect = secrect
+        self.agentid = agentid
+
     def get_access_token(self):
-        access_token_url="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid="+self.corpid+"&corpsecret="+self.secrect
+        access_token_url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + \
+            self.corpid+"&corpsecret="+self.secrect
         try:
-            res_data = urllib2.urlopen(access_token_url,timeout=3)
-            access_token=json.loads(res_data.read())["access_token"]
-        except Exception,e:
+            res_data = urllib2.urlopen(access_token_url, timeout=3)
+            access_token = json.loads(res_data.read())["access_token"]
+        except Exception as e:
             logging.info(str(e))
             logging.info("access_token获取超时")
-            return None 
+            return None
         else:
             return access_token
+
     def check_token(self):
         if os.path.exists("/tmp/"+str(self.Md5value())):
-            with open("/tmp/"+str(self.Md5value()),"r+") as fd:
-	        result_info=fd.read().split("^")
-                timestamp=result_info[1]
-                if time.time()-int(timestamp) <7200:
-                    access_token=result_info[0]
-		    return access_token
+            with open("/tmp/"+str(self.Md5value()), "r+") as fd:
+                result_info = fd.read().split("^")
+                timestamp = result_info[1]
+                if time.time()-int(timestamp) < 7200:
+                    access_token = result_info[0]
+                    return access_token
                 else:
-                    access_token=self.get_access_token()
-                    timestamp=time.time()
-                    tokentime=access_token+"^"+str(timestamp).split(".")[0]
-                    with open("/tmp/"+str(self.Md5value()),"w") as fd:
+                    access_token = self.get_access_token()
+                    timestamp = time.time()
+                    tokentime = access_token+"^"+str(timestamp).split(".")[0]
+                    with open("/tmp/"+str(self.Md5value()), "w") as fd:
                         fd.write(tokentime)
-		    return access_token
+                    return access_token
         else:
-            access_token=self.get_access_token()
-            timestamp=time.time()
-            tokentime=access_token+"^"+str(timestamp).split(".")[0]
-            with open("/tmp/"+str(self.Md5value()),"w") as fd:
+            access_token = self.get_access_token()
+            timestamp = time.time()
+            tokentime = access_token+"^"+str(timestamp).split(".")[0]
+            with open("/tmp/"+str(self.Md5value()), "w") as fd:
                 fd.write(tokentime)
-	    return access_token
+            return access_token
+
     def Md5value(self):
         m2 = hashlib.md5()
         m2.update(str(self.agentid)+str(self.corpid)+str(self.secrect))
+
         return m2.hexdigest()
-    def sendmsg(self,title,description):
+
+    def sendmsg(self, title, description):
+        response = None
         try:
-	    access_token=self.check_token()
+            access_token = self.check_token()
             if access_token:
-                send_url="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="+access_token
-                send_info={
-	        	"touser" : "@all",
-	        	"msgtype" : "text",
-	        	"agentid" : self.agentid,
-                "text":{
-                    "content":str(title)+":"+str(description)
+                send_url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="+access_token
+                send_info = {
+                    "touser": "@all",
+                    "msgtype": "text",
+                    "agentid": self.agentid,
+                    "text": {
+                        "content": str(title)+":"+str(description)
                     }
-	        	}
+                }
                 logging.info(send_info)
                 send_info_urlencode = json.dumps(send_info)
                 #send_info_urlencode = json.dumps(send_info,ensure_ascii=False)
-                req=urllib2.Request(url = send_url,data =send_info_urlencode)
-                response=urllib2.urlopen(req,timeout=3)
-                res_info=response.read()
+                req = urllib2.Request(url=send_url, data=send_info_urlencode)
+                response = urllib2.urlopen(req, timeout=3)
+                res_info = response.read()
             else:
                 logging.error("no access_token")
-        except Exception,e:
+        except Exception as e:
             logging.error(str(e))
         else:
-            alarm_result=json.loads(res_info)
-            if int(alarm_result["errcode"])==0:
+            alarm_result = json.loads(res_info)
+            if int(alarm_result["errcode"]) == 0:
                 logging.info("报警正常")
             else:
                 logging.info(alarm_result["errmsg"])
         finally:
             if response:
                 response.close()
+
+
+if __name__ == "__main__":
+    wx_ins = weixinalarm('wx24dc164efd94b180',
+                         'pXQlHFcyRWRQzq3T4Hiy7MkTYP6ZpYrhRxfCZQuDZDU', 1000009)
+    wx_ins.sendmsg("hah", "hah")
